646415d30c421c854d0998c7ba8b9d5d9bfd4b73
ahmedEid1 <kingmedo954@gmail.com>
2021-04-10 23:47:34 +0200

    inital TODOs for the toolbox feature T_T

---
+++ js/components/toolbox.js
@@ -0,0 +1,5 @@
+// TODO: Round 1
+// TODO: create the button >> make it absolute >> position it to the left-bottom of the page
+// TODO: make the button draggable to any place else in the screen
+// TODO: add the style(color, rounded, hover, ...) and the icon inside the button
+// TODO: add the feature to open and collapse the toolBox on click
