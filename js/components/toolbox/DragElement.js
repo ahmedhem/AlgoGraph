4aee3fbdaba71a0bc9d3dc3965fb87583098ab3f
ahmedEid1 <kingmedo954@gmail.com>
2021-04-11 23:38:51 +0200

    adding the toolbox toggler button and make it draggable

---
+++ js/components/toolbox/DragElement.js
@@ -0,0 +1,44 @@
+function dragElement(draggableElement) {
+    let newPositionX = 0, newPositionY = 0, oldPositionX = 0, oldPositionY = 0;
+
+    draggableElement.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e.preventDefault();
+
+        // get the mouse cursor position at startup:
+        oldPositionX = e.clientX;
+        oldPositionY = e.clientY;
+
+        // stop the dragging when the user stop clicking
+        document.onmouseup = closeDragElement;
+
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e.preventDefault();
+
+        // calculate the new cursor position:
+        newPositionX = oldPositionX - e.clientX;
+        newPositionY = oldPositionY - e.clientY;
+
+        oldPositionX = e.clientX;
+        oldPositionY = e.clientY;
+
+        // set the element's new position:
+        draggableElement.style.top = (draggableElement.offsetTop - newPositionY) + "px";
+        draggableElement.style.left = (draggableElement.offsetLeft - newPositionX) + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+            // remove the listeners
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+
+}
+
+export default dragElement;
---
+++ js/components/toolbox/toolbox.js
@@ -0,0 +1,7 @@
+// TODO: Round 1 >> create the button >> make it absolute >> position it to the right-bottom of the page (done) >> make the button draggable to any place else in the screen (done) >>add the style(color, rounded, hover, ...) and the icon inside the button (done)
+// TODO: add the feature to open and collapse the toolBox on click
+
+import dragElement from "./DragElement.js";
+
+dragElement(document.getElementById("toolbox-toggle"));
+
