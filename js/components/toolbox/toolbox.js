c0adaad914f411893bdcf67b5d4e2a2bcc0ddb58
ahmedEid1 <kingmedo954@gmail.com>
2021-04-17 18:19:22 +0200

    adding the animation of clicking the toggler

--- js/components/toolbox/toolbox.js
+++ js/components/toolbox/toolbox.js
@@ -1,7 +1,49 @@
-// TODO: Round 1 >> create the button >> make it absolute >> position it to the right-bottom of the page (done) >> make the button draggable to any place else in the screen (done) >>add the style(color, rounded, hover, ...) and the icon inside the button (done)
 // TODO: add the feature to open and collapse the toolBox on click
 
-import dragElement from "./DragElement.js";
+import dragElement from "./utils/DragElement.js";
 
-dragElement(document.getElementById("toolbox-toggle"));
+
+const toggleButton = document.querySelector(".draggable-button");
+
+// makes the button draggable (the element must be absolute)
+dragElement(toggleButton);
+
+
+// differentiate mouse “click” and “drag” event
+let drag = false;
+
+document.addEventListener(
+    'mousedown', () => drag = false);
+
+document.addEventListener(
+    'mousemove', () => drag = true);
+
+document.addEventListener(
+    'mouseup', () => console.log(
+        drag ? 'drag' : 'click'));
+
+
+
+function hide_button(button) {
+   if (!drag) {
+       // remove the current position
+       button.style.removeProperty('top');
+       button.style.removeProperty('left');
+
+       // add the new position >> change the place and add the animation fade out
+       button.classList.add('button-fade');
+
+   }
+}
+
+const show_toolbox = () => {
+    // hide the button
+    hide_button(toggleButton);
+
+    //
+}
+
+// when i click the button >> go to the middle of the right side of the screen >> fade away
+document.addEventListener('click', show_toolbox);
+
 
