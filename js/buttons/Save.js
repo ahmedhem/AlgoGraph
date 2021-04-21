03824fd698682c86841d678cd12b190ecf01f8c2
ahmedEid1 <kingmedo954@gmail.com>
2021-04-21 14:08:12 +0200

    save the file as .txt

--- js/buttons/Save.js
+++ js/buttons/Save.js
@@ -1,27 +1,67 @@
-const saveButton = document.querySelector('.save');
-saveButton.addEventListener('click', (e) => saveClickedHandler(e));
+function convert_graph_into_text(graph) {
+    const nodeCount = "Nodes count: " + (graph.nodeCount - 1) + '\n';
 
+    let edges = '';
+    let edgesCount = 0;
+    graph.nodes.forEach(node => {
+        node.edges.forEach(edge => {
+            edges += edge + '\n';
+            edgesCount++;
+        })
+    });
 
-const saveClickedHandler = (e) => {
-    save_graph_as_img();
+    edgesCount = "Edges count: " + edgesCount + "\n";
+
+    return  nodeCount + edgesCount + edges;
 }
+
+function download_file(fileName, fileUrl) {
+    const a = document.createElement('a');
+    a.href= fileUrl;
+    a.download = fileName;
+    a.click();
+
+    URL.revokeObjectURL(a.href);
+}
+
+
+// save as image
+const saveImgButton = document.querySelector('.save-as-img');
+saveImgButton.addEventListener('click', save_graph_as_img);
 
 
 function save_graph_as_img() {
-    // create an img form the canvas
-    const img = UI.canvas.toDataURL("image/png");
+    try {
+        const url = UI.canvas.toDataURL("image/png");
 
-    // add the img as a downloadable Link to a div with display: none
-    const downloadDiv = document.querySelector('#download');
-    downloadDiv.innerHTML = `<a download="graph.png" href="${img}" title="ImageName"></a>`;
+        download_file('graph.png', url);
 
-    // click the link automatically to download the img
-    document.querySelector("#download a").click();
-
-    // remove the link from the div after downloading
-    downloadDiv.innerHTML = "";
+        return true;
+    } catch (error) {
+        console.error("Could not download the img for some reason:", error);
+        return false;
+    }
 
 }
 
+// save as .txt
+const saveTxtButton = document.querySelector('.save-as-txt');
+saveTxtButton.addEventListener('click', save_graph_as_txt);
 
 
+function save_graph_as_txt() {
+    let graph_text = convert_graph_into_text(graph);
+
+    try {
+        const file = new Blob([graph_text], {type: 'text/plain'});
+        const url = URL.createObjectURL(file);
+
+        download_file('graph.txt', url);
+
+        return true;
+    } catch (error) {
+        console.error("Could not download the text for some reason:", error);
+        return false;
+    }
+
+}
