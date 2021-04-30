2165dc52461d21ecd08078bcbace24edb127b92a
ahmedEid1 <kingmedo954@gmail.com>
2021-04-30 18:04:15 +0200

    Sidebar and canvas changes: - refactored the sidebar css file - refactor the sidebar html file - change the canvas width and height

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 //...Initializing the UI and the Canvas variables
 const the_canvas = document.getElementById('main_canvas');
 if (the_canvas && the_canvas.getContext) {
-    the_canvas.width = .80 * window.innerWidth;
-    the_canvas.height = .75 * window.innerHeight;
+    the_canvas.width = window.innerWidth;
+    the_canvas.height = window.innerHeight;
 
     const ctx = the_canvas.getContext('2d');
     if (ctx) {
