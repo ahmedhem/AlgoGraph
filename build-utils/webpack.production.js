6a7d1eb7da585d5154d5bfebece3c7630b820bfc
Ahmed Magdy <ama@luftborn.com>
2022-05-30 00:47:15 +0200

    Build dist path has been fixed and CircleCi integration added

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -5,7 +5,7 @@ module.exports = ({
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../', 'dist'),
   },
   module: {
     rules: [
